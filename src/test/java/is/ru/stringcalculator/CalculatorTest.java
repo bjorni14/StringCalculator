9d8e1c4f806bbb9c12e02e26d765e2123120aca0
bjorni14 <bjorni14@ru.is>
2015-10-13 15:47:29 +0000

    Revert "Revert "negative exception added""
    This reverts commit 2aa3d4303557ccbc35bf9318def70fd3e670550b.

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -9,29 +9,54 @@ public class CalculatorTest {
       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
+    //1
 	@Test
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
 
+	//2
 	@Test
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
 
+	//3
 	@Test
 	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
 	}	
 
+	//4
 	@Test
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
+    //5
     @Test
     public void testNewLine(){
     	assertEquals(6, Calculator.add("1,2\n3")); 
+    }
+
+    /*//6
+    @Test
+    public void testDiffDelimeter(){
+    	assertEquals(5, Calculator.add("//;2\n3")); 
+    }*/
+
+    //7
+    @Test
+    public void testNegativNumber()
+    {
+    	try
+    	{
+    		Calculator.add("2,-2,6,-5");
+    	}
+    	catch (RuntimeException ex)
+    	{
+    		assertEquals("Negatives not allowed: -2,-5", ex.getMessage());
+    	}
     }
 
 }
