25d3dd3c04e850735082aef172b202f2ba8a49ad
bjorni14 <bjorni14@ru.is>
2015-10-13 15:56:16 +0000

    negative and number over 1000

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,29 +2,62 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
-	public static int add(String text){
-		if(text.equals("")){
+	public static int add(String text)
+	{
+		if(text.equals(""))
+		{
 			return 0;
 		}
-		else if(text.contains(",") || text.contains("\n")){
+
+		if(text.contains("-"))
+		{
+			negativeNumbers(text);
+		}
+
+		//if something like this //
+
+		if(text.contains(",") || text.contains("\n"))
+		{
 			return sum(splitNumbers(text));
 		}
 		else
 			return 1;
 	}
 
-	private static int toInt(String number){
+	private static int toInt(String number)
+	{
 		return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
+	private static String[] splitNumbers(String numbers)
+	{
 	    return numbers.split(",|\n");
 	}
-      
-    private static int sum(String[] numbers){
+
+	private static void negativeNumbers(String numbers) throws IllegalArgumentException
+	{
+		String[] text = numbers.split("-");
+		String message = "Negatives not allowed: ";
+		for(int i = 1; i < text.length; i++)
+		{
+			message = message + "-" + text[i].substring(0,1);
+			if(i < text.length - 1)
+			{
+				message = message + ",";
+			}
+		}
+		throw new IllegalArgumentException(message);
+	}
+
+    private static int sum(String[] numbers)
+    {
  	    int total = 0;
-        for(String number : numbers){
-		    total += toInt(number);
+        for(String number : numbers)
+        {
+        	if(toInt(number) <= 1000)
+        	{
+        		total = total + toInt(number);
+        	}
 		}
 		return total;
     }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -5,33 +5,72 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) 
+	{
       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
+    //1
 	@Test
-	public void testEmptyString() {
+	public void testEmptyString() 
+	{
 		assertEquals(0, Calculator.add(""));
 	}
 
+	//2
 	@Test
-	public void testOneNumber() {
+	public void testOneNumber() 
+	{
 		assertEquals(1, Calculator.add("1"));
 	}
 
+	//3
 	@Test
-	public void testTwoNumbers() {
+	public void testTwoNumbers() 
+	{
 		assertEquals(3, Calculator.add("1,2"));
 	}	
 
+	//4
 	@Test
-    public void testMultipleNumbers(){
+    public void testMultipleNumbers()
+    {
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
+    //5
     @Test
-    public void testNewLine(){
+    public void testNewLine()
+    {
     	assertEquals(6, Calculator.add("1,2\n3")); 
+    }
+
+    /*//6
+    @Test
+    public void testDiffDelimeter()
+    {
+    	assertEquals(5, Calculator.add("//;2\n3")); 
+    }*/
+
+    //7
+    @Test
+    public void testNegativNumber()
+    {
+    	try
+    	{
+    		Calculator.add("2,-2,6,-5");
+    	}
+    	catch (RuntimeException ex)
+    	{
+    		assertEquals("Negatives not allowed: -2,-5", ex.getMessage());
+    	}
+    }
+
+    //8
+    @Test
+    public void testToBigNumber()
+    {
+    	assertEquals(8, Calculator.add("1,2,1001,5,2503")); 
     }
 
 }
